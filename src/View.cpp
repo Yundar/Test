214e2e49297bb8bf7ec9b73461eee8d494ca79b3
Yurii <Yurri2002@gmail.com>
2021-10-01 12:18:55 +0300

    aww

--- src/View.cpp
+++ src/View.cpp
@@ -69,7 +69,7 @@ void View::drawMap(Board board) {
                 window.close();
             }
             if (event.type == sf::Event::MouseButtonPressed){
-                if (IntRect(x, y, CELL_SIZE, CELL_SIZE).contains(pos.x, pos.y)){
+                if (sf::IntRect(x, y, CELL_SIZE, CELL_SIZE).contains(pos.x, pos.y)){
                     moves = m_model->getPossibleMoves();
                     std::cout << "That's ok" << std::endl;
                     for (unsigned int i = 0; i < moves.size(); i++){
