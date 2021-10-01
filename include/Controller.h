9c5f92c2aef305898a9b47bd05949c9e8b17586d
Yurii <Yurri2002@gmail.com>
2021-10-01 22:11:27 +0300

    ge

--- include/Controller.h
+++ include/Controller.h
@@ -3,14 +3,11 @@
 
 #include "Game.h"
 #include "View.h"
-#include <SFML/Graphics/RenderWindow.hpp>
 
 class Controller {
 private:
     Game *m_model;
     View *m_view;
-
-    sf::RenderWindow window;
 
 public:
     Controller(Game *model, View *view);
