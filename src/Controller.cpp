5946a821147cc59997aac10755fbd88dab2c5b2d
Yurii <Yurri2002@gmail.com>
2021-10-02 00:56:27 +0300

    test

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -12,43 +12,42 @@ Controller::Controller(Game *model, View *view) {
 
 void Controller::start() {
     sf::Window *window = m_view->getWindow();
-    while (true) {
-        try {
-            if (m_model->checkGameEnd() == true) break;
-            
-            
+    try {
+        if (m_model->checkGameEnd() == true) break;
+        
+        
 
-            while (window->isOpen()){
-                if (m_model->getCurrentPlayerNeedsInput()) {
-                    int x, y;
-                    m_model->getCurrentPlayerPosition(&x, &y);
+        while (window->isOpen()){
+            if (m_model->getCurrentPlayerNeedsInput()) {
+                int x, y;
+                m_model->getCurrentPlayerPosition(&x, &y);
 
-                    sf::Vector2i pixelPos = sf::Mouse::getPosition(*window);
-                    sf::Event event;
-                    while(window->pollEvent(event)){
-                        if (event.type == sf::Event::Closed){
-                            window->close();
+                sf::Vector2i pixelPos = sf::Mouse::getPosition(*window);
+                sf::Event event;
+                while(window->pollEvent(event)){
+                    if (event.type == sf::Event::Closed){
+                        window->close();
+                    }
+                    if (event.type == sf::Event::MouseButtonPressed){
+                        if (sf::IntRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE).contains(pixelPos.x, pixelPos.y)){
+                            m_view->drawPossibleMoves();
+                        } else{
+                            m_model->makeTurn(pixelPos.x / CELL_SIZE, pixelPos.y / CELL_SIZE);
                         }
-                        if (event.type == sf::Event::MouseButtonPressed){
-                            if (sf::IntRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE).contains(pixelPos.x, pixelPos.y)){
-                                m_view->drawPossibleMoves();
-                            } else{
-                                m_model->makeTurn(pixelPos.x / CELL_SIZE, pixelPos.y / CELL_SIZE);
-                            }
-                            for (unsigned int i = 0; i < m_model->getPossibleMoves().size(); i++){
-                                if (sf::IntRect(m_model->getPossibleMoves()[i].first * CELL_SIZE, m_model->getPossibleMoves()[i].second * CELL_SIZE, CELL_SIZE, CELL_SIZE).contains(pixelPos.x, pixelPos.y)){
-                                    m_model->makeTurn(m_model->getPossibleMoves()[i].first, m_model->getPossibleMoves()[i].second);
-                                }
+                        for (unsigned int i = 0; i < m_model->getPossibleMoves().size(); i++){
+                            if (sf::IntRect(m_model->getPossibleMoves()[i].first * CELL_SIZE, m_model->getPossibleMoves()[i].second * CELL_SIZE, CELL_SIZE, CELL_SIZE).contains(pixelPos.x, pixelPos.y)){
+                                m_model->makeTurn(m_model->getPossibleMoves()[i].first, m_model->getPossibleMoves()[i].second);
                             }
                         }
                     }
-                }else {
-                    m_model->makeTurn(0, 0);
                 }
-            } 
-            
-        } catch(const std::exception& e) {
-            std::cerr << e.what() << "\n\n";
-        }
+            }else {
+                m_model->makeTurn(0, 0);
+            }
+        } 
+        
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << "\n\n";
     }
+
 }
