aebd75fb30061f6edf727f97287dab8ece81c84b
Yurii <Yurri2002@gmail.com>
2021-10-02 20:18:51 +0300

    wfa

--- src/Launcher.cpp
+++ src/Launcher.cpp
@@ -8,7 +8,8 @@ Launcher::Launcher(bool playAgain){
 
 	bool again = playAgain;
 
-	sf::RenderWindow window(sf::VideoMode(1376, 768), "Quoridor");	
+	sf::RenderWindow window(sf::VideoMode(1376, 768), "Quoridor");
+	window.clear(sf::Color(129, 181, 221));		
 
 
     singlePlayerTexture.loadFromFile("./media/SinglePlayer.png");
@@ -62,7 +63,6 @@ void Launcher::menu(){
     while (isMenu)
 	{
 
-		window.clear(sf::Color(129, 181, 221));	
 		singePlayer.setColor(sf::Color::White);
 		multiplayer.setColor(sf::Color::White);
 		exit.setColor(sf::Color::White);
