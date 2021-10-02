9441fa1ff5483e0dc975d33c87bbceaffa059e0f
Yurii <Yurri2002@gmail.com>
2021-10-02 20:25:28 +0300

    awd

--- src/Launcher.cpp
+++ src/Launcher.cpp
@@ -70,9 +70,9 @@ void Launcher::menu(){
 		menuNum = 0;
 
  
-		if (sf::IntRect(100, 30, 229, 43).contains(sf::Mouse::getPosition(window))) { singePlayer.setColor(sf::Color::Blue); menuNum = 1; }
-		if (sf::IntRect(100, 90, 207, 41).contains(sf::Mouse::getPosition(window))) { multiplayer.setColor(sf::Color::Blue); menuNum = 2; }
-		if (sf::IntRect(100, 150, 72, 37).contains(sf::Mouse::getPosition(window))) { exit.setColor(sf::Color::Blue); menuNum = 3; }
+		if (sf::IntRect(100, 30, 229, 43).contains(sf::Mouse::getPosition(window))) { singePlayer.setColor(sf::Color::Red); menuNum = 1; }
+		if (sf::IntRect(100, 90, 207, 41).contains(sf::Mouse::getPosition(window))) { multiplayer.setColor(sf::Color::Red); menuNum = 2; }
+		if (sf::IntRect(100, 150, 72, 37).contains(sf::Mouse::getPosition(window))) { exit.setColor(sf::Color::Red); menuNum = 3; }
  
 		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 		{
