f51e2429d3217afdeb6fdb3a990f254d12218447
Yurii <Yurri2002@gmail.com>
2021-10-03 18:51:39 +0300

    adwfesgrhjyk

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -27,11 +27,11 @@ void Controller::start() {
                         if (sf::IntRect(x * CELL_SIZE - border, y * CELL_SIZE, CELL_SIZE, CELL_SIZE).contains(pixelPos.x, pixelPos.y)){
                             m_view->drawPossibleMoves();
                         } else{
-                            m_model->makeTurn(pixelPos.x / CELL_SIZE, pixelPos.y / CELL_SIZE);
+                            m_model->makeTurn((pixelPos.x - border)/ CELL_SIZE, pixelPos.y / CELL_SIZE);
                         }
                         for (unsigned int i = 0; i < m_model->getPossibleMoves().size(); i++){
-                            if (sf::IntRect(m_model->getPossibleMoves()[i].first * CELL_SIZE, m_model->getPossibleMoves()[i].second * CELL_SIZE, CELL_SIZE, CELL_SIZE).contains(pixelPos.x, pixelPos.y)){
-                                m_model->makeTurn(m_model->getPossibleMoves()[i].first, m_model->getPossibleMoves()[i].second);
+                            if (sf::IntRect(m_model->getPossibleMoves()[i].first - border * CELL_SIZE, m_model->getPossibleMoves()[i].second * CELL_SIZE, CELL_SIZE, CELL_SIZE).contains(pixelPos.x, pixelPos.y)){
+                                m_model->makeTurn(m_model->getPossibleMoves()[i].first - border, m_model->getPossibleMoves()[i].second);
                             }
                         }
                     }
