dadaccd762d29562d9c697124819e0ed35277a32
Yurii <Yurri2002@gmail.com>
2021-10-04 17:04:34 +0300

    ??????????

--- src/Launcher.cpp
+++ src/Launcher.cpp
@@ -19,7 +19,7 @@ void Launcher::mainMenu(){
    	window.clear(backgroundColor);
 
 	fadeRect.setFillColor(sf::Color::Black);
-	fadeRect.setSize(windowSize); //вместо 1280*720 - свои размеры окна, а еще лучше брать их из window.getSize()
+	fadeRect.setSize(windowWidth, windowHeight); //вместо 1280*720 - свои размеры окна, а еще лучше брать их из window.getSize()
 	
 	sf::Clock fadeClock;
 	int alpha = 0;
