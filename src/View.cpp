f13ebde43b8e0815282ad1740faba75bdb3013d3
Yurii <Yurri2002@gmail.com>
2021-11-01 13:26:47 +0200

    some changes in array

--- src/View.cpp
+++ src/View.cpp
@@ -23,7 +23,7 @@ void View::drawMap(Board board) {
     m_model->getSecondPlayerPosition(&x2, &y2);
 
     char top[17] = {'A', 'S', 'B', 'T', 'C', 'U', 'D', 'V', 'E', 'W', 'F', 'X', 'G', 'Y', 'H', 'Z', 'I'};
-    char cells[9] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+    std::string cells[9] = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
 
     std::cout << "    ";
     for (int i = 0; i < mapSize; i++) {
