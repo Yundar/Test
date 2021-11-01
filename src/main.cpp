2daf4d9e20e52ab259870336119992f4a79cbb2d
Yurii <Yurri2002@gmail.com>
2021-11-01 12:18:26 +0200

    changes in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,26 @@
-// #include "Launcher.h"
-
-// #include <ctime> // time()
-// #include <cstdlib> // rand(), srand()
-
-// // TODO: Slide show in launcher
-// // TODO: Input names in launcher
-// // TODO: Config file 
-// // TODO: show errors in GUI
-
-// int main() {
-//     srand(time(0));
-    
-//     Launcher launcher;
-
-//     do {
-//         launcher.mainMenu();
-//     } while (launcher.exitMenu());
-
-//     return 0;
-// }
 #include "Game.h"
 #include "View.h"
 #include "Controller.h"
 
 #include <ctime> // time()
 #include <cstdlib> // rand(), srand()
+#include <string>
 
 void single(const char* firstName, const char* secondName);
-void multi(const char* firstName, const char* secondName);
 
 int main() {
     srand(time(0));
 
-    bool playAgain = false;
+    std::cout << "What collor you want to play? white/black" << std::endl;
+    std::string answer;
+    std::cin >> answer;
 
-    do {
-        std::cout << "You want to play alone or with a friend? S/m" << std::endl;
-        char answer = 's';
-        std::cin >> answer;
-
-        if (answer == 's') single("Player", "Wally");
-        else multi("Player1", "Player2");
-
-        std::cout << "Want to play again? y/N" << std::endl;
-        std::cin >> answer;
-        if (answer == 'y') playAgain = true;
-        else playAgain = false;
-        std::cout << std::endl;
-
-    } while (playAgain);
+    if (answer == "white"){
+        single("Player", "Wally");
+    } else if (answer == "black"){
+        single("Wally", "Player");
+    }
+    
 
     return 0;
 }
@@ -56,16 +28,6 @@ int main() {
 void single(const char* firstName, const char* secondName) {
     Player fp(mapSize/2, mapSize-1, firstName);
     Bot sp(mapSize/2, 0, secondName);
-
-    Game game(fp, sp);
-    View view(&game);
-    Controller controller(&game);
-    controller.start();
-}
-
-void multi(const char* firstName, const char* secondName) {
-    Player fp(6, 8, firstName);
-    Player sp(8, 8, secondName);
 
     Game game(fp, sp);
     View view(&game);
